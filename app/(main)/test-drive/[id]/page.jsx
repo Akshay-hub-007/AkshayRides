260b1e08a3053bdf4b14bf3028b3f4314994f608
Akshay-hub-007 <akshaykalangi9@gmail.com>
2025-06-25 23:51:04 +0530

    Refactor TestDriveForm component to include validation schema and state management

--- app/(main)/test-drive/[id]/page.jsx
+++ app/(main)/test-drive/[id]/page.jsx
@@ -4,31 +4,29 @@ import React from 'react'
 import TestDriveForm from '../_components/test-drive-form'
 
 
-export async function generateMetaData()
-{
-   return {
-    title:`Book  a test drive | AkshayCarverse`,
-    description:` Schedule a test drive in few seconds`
-   }
+export async function generateMetaData() {
+  return {
+    title: `Book  a test drive | AkshayCarverse`,
+    description: ` Schedule a test drive in few seconds`
+  }
 }
-async function page({params}) {
-  
-  const {id}=params
+async function page({ params }) {
 
-  const result= await getCarById(id)
+  const { id } = await params
 
-  if(!result.success)
-  {
+  const result = await getCarById(id)
+
+  if (!result.success) {
     notFound()
   }
-  
+
   return (
     <div className='container mx-auto px-4 py-12'>
 
       <h1 className='text-6xl mb-6  gradient-title'>
- Book a test drive
+        Book a test drive
       </h1>
-       <TestDriveForm/>
+      <TestDriveForm />
     </div>
   )
 }
--- app/(main)/test-drive/_components/test-drive-form.jsx
+++ app/(main)/test-drive/_components/test-drive-form.jsx
@@ -1,9 +1,43 @@
+"use client"
+import { useRouter } from 'next/router'
 import React from 'react'
+import { z } from 'zod'
 
-function TestDriveForm() {
+
+const testDriveSchema = z.object({
+  date: z.date({
+    required_error: "Please select a date for your test drive",
+  }),
+  timeSlot: z.string({
+    required_error: "Please select a time slot"
+  }),
+  notes: z.string().optional()
+})
+
+function TestDriveForm({car,testDriveInfo}) {
+  const router = useRouter();
+  const [availableTimeSlots, setAvailableTimeSlots] = useState([]);
+  const [showConfirmation, setShowConfirmation] = useState(false);
+  const [bookingDetails, setBookingDetails] = useState(null);
+ 
+     const {
+    control,
+    handleSubmit,
+    watch,
+    setValue,
+    reset,
+    formState: { errors, isValid },
+  } = useForm({
+    resolver: zodResolver(testDriveSchema),
+    defaultValues: {
+      date: undefined,
+      timeSlot: undefined,
+      notes: "",
+    },
+  });
   return (
     <div>
-      
+
     </div>
   )
 }
